2fa974bcf51795721b4b27b627f9d490ca52c60d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-01-31 18:15:41 +0200

    js aritmetika

--- Javascript/Jan30/index.js
+++ Javascript/Jan30/index.js
@@ -1,1 +1,16 @@
-console.log("swx");
+let x = 50;
+let y = 10;
+
+let suma = x + y;
+let skirtumas = x - y;
+let daugyba = x * y; 
+let dalyba = x / y;
+
+
+// Rezultatu rinkinys
+let rezultatai = [suma, skirtumas, daugyba, dalyba];
+
+//Parodyti visus rezultaus
+for(i=0; i<4; i++){
+    console.log(rezultatai[i]);
+}
