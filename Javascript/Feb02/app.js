ee0369a98263857e2dc06703f03161462a5d36f0
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-02 21:06:12 +0200

    function parameters and alert boxes

--- Javascript/Feb02/app.js
+++ Javascript/Feb02/app.js
@@ -1,40 +1,74 @@
+
+// //////////////////////////////////////////
+// //            Funcijos Parametrai       //
+// //////////////////////////////////////////
+
+
+
+
+getAge(19);
+
+// Ternary versija
+function getAge(ageIn) {
+  return ageIn >= 18 ? alert("Galima gerti!") : confirm("Ar tevai leido gerti?");
+}
+
+// pirma versija
+
+// function getAge (ageIn){
+//   if(ageIn >= 18){
+//     alert("Galima");
+//   }else{
+//     confirm("Ar tevai leido gerti?");
+//   }
+// }
+
+
+
+
+// sum(2,2);
+
+// function sum (x, y){
+//   return console.log(x + y);
+// }
+
+
 // //////////////////////////////////////////
 // //            Forma Js                  //
 // //////////////////////////////////////////
 
 
+
+// const userForm = document.getElementById("form");
+// userForm.addEventListener("submit", onSubmitForm);
+
+// let userName = "";
+// let userLastName = "";
+// let here = document.getElementById("here");
+
+// function onSubmitForm(e) {
+//   // Prevent Refresh
+//   e.preventDefault();
+//   //   Grab inputs by their values
+//   userName = e.target.elements.userName.value;
+//   userLastName = e.target.elements.userLastName.value;
+
+//   //   Console check
+//   console.log(userName, userLastName);
+
+//   //   print out stuff to web
+//   here.textContent = `Sveiki, mano vardas yra ${userName + " " + userLastName}.`;
+// }
+
+
+// PIRMA DALIS UZDUOTIES
 // userForm.addEventListener("submit", function onSubmitForm() {
-    //     console.log("Form Submited Successfully!");
-    // });
-    
-    const userForm = document.getElementById("form");
-    userForm.addEventListener("submit", onSubmitForm);
-    
-    let userName = "";
-    let userLastName = "";
-    let here = document.getElementById("here");
-    
-    function onSubmitForm(e) {
-        // Prevent Refresh
-      e.preventDefault();
-    //   Grab inputs by their values
-      userName = e.target.elements.userName.value;
-      userLastName = e.target.elements.userLastName.value;
-
-    //   Console check
-      console.log(userName, userLastName);
-
-    //   print out stuff to web
-      here.textContent = userName + userLastName;
-    }
-
-
-
+//     console.log("Form Submited Successfully!");
+// });
 
 // //////////////////////////////////////////
 // //            Antra                     //
 // //////////////////////////////////////////
-
 
 // let figura = document.querySelector(".figura");
 
@@ -42,20 +76,14 @@
 //     figura.style.display = 'none';
 //   });
 
-
-
-
-
 // //////////////////////////////////////////
 // //            Pirma                     //
 // //////////////////////////////////////////
-
 
 // let pirmas = document.querySelector("#change");
 // let antras = document.querySelector("#add");
 // let p = document.querySelector("#color");
 // let p2 = document.querySelector("#insert");
-
 
 // // Ketvirtas
 // function insert (){
