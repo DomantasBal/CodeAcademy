82999c8ea8f868ea8008737618cc0a9240f18973
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-02 18:42:49 +0200

    event listeners and on click stuff

---
+++ Javascript/Feb02/app.js
@@ -0,0 +1,52 @@
+// //////////////////////////////////////////
+// //            Antra                     //
+// //////////////////////////////////////////
+
+
+let figura = document.querySelector(".figura");
+
+figura.addEventListener('mouseover', function() {
+    figura.style.display = 'none';
+  });
+
+
+
+
+
+
+
+
+
+
+// //////////////////////////////////////////
+// //            Pirma                     //
+// //////////////////////////////////////////
+
+
+// let pirmas = document.querySelector("#change");
+// let antras = document.querySelector("#add");
+// let p = document.querySelector("#color");
+// let p2 = document.querySelector("#insert");
+
+
+// // Ketvirtas
+// function insert (){
+//     p2.innerHTML = "Tekstas tekstas"
+// }
+
+// // TRECIAS
+// function color (){
+//     p.style.color = "red";
+
+// }
+
+// // ANTRAS
+// function append (){
+//     antras.innerHTML += " pridetas tekstas ";
+
+// }
+
+// // PIRMAS
+// function changeText(){
+//     pirmas.textContent = "Pakeistas Tekstas";
+// }
