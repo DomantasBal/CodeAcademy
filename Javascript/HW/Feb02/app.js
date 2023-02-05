201e72b3cbb53ace593b33651e30cdbf1e64b28d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-05 15:12:08 +0200

    ex.9

---
+++ Javascript/HW/Feb02/app.js
@@ -0,0 +1,27 @@
+// Ex. NR 9
+
+
+
+
+let show = () => {
+    console.log("Anonymous function");
+};
+
+show();
+
+
+let add = (a, b) => {return a + b};
+
+console.log(add(2,2));
+
+
+/////////// ONE /////////////
+// let show = function() {
+//     console.log("Anonymous function");
+//   };
+
+/////////// TWO /////////////
+//   let add = function(a, b) {
+//     return a + b;
+//   };
+  
