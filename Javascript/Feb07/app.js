a972cabe3d5acf45c43864e07d241aed1bf985e6
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-07 19:00:38 +0200

    Sugeneruoti html vien su js ir pasiimti inputa ir panaudoti array metodus - finished

---
+++ Javascript/Feb07/app.js
@@ -0,0 +1,80 @@
+// //////////////////////////////////////////
+//                                         //
+// //////////////////////////////////////////
+const btnAdd = document.querySelector("#add")
+const btnPush = document.querySelector("#push")
+const btnPop = document.querySelector("#pop")
+const btnShift = document.querySelector("#last")
+
+
+// Input element
+let input = document.createElement ("input");
+input.type = "text";
+
+// BUTTON Prideti
+let buttonAdd = document.createElement("button");
+buttonAdd.id = "add";
+buttonAdd.innerText = "Prideti";
+buttonAdd.onclick = add;
+
+// BUTTON Prideti i gala
+let buttonPush = document.createElement("button");
+buttonPush.id = "push";
+buttonPush.innerText = "Prideti i gala";
+buttonPush.onclick = push;
+
+//BUTTON Isimti paskutini
+let buttonPop = document.createElement("button");
+buttonPop.id = "pop";
+buttonPop.innerText = "Isimti paskutini";
+buttonPop.onclick = pop;
+
+//BUTTON Isimti pirma
+let buttonLast = document.createElement("button");
+buttonLast.id = "last";
+buttonLast.innerText = "Isimti pirma";
+buttonLast.onclick = last;
+
+
+let p = document.createElement ("p");
+
+let array = ["   Vienas", "Du", "Trys", "Keturi"]
+
+
+
+
+function add(){
+
+
+    array.unshift(input.value)
+    document.body.append(array);
+}
+
+function push(){
+    array.push(input.value)
+    document.body.append(array);
+}
+
+function pop(){
+    array.pop(input.value)
+    document.body.append(array);
+}
+
+function last(){
+    array.shift(input.value)
+    document.body.append(array);
+}
+
+
+//Rodyti elementus
+document.body.append(input, buttonAdd,buttonPush,buttonPop,buttonLast)
+
+
+
+
+
+
+
+
+
+
