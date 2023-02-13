2dae66da0ed7a59298434f0f54f87c5b82609026
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-13 19:53:51 +0200

    constr.

--- Javascript/Feb13/app.js
+++ Javascript/Feb13/app.js
@@ -1,39 +1,75 @@
 // //////////////////////////////////////////
+//               FORM                      //
+// //////////////////////////////////////////
+
+
+function Movie(name, year, director, budget, income) {
+    this.name = name;
+    this.year = year;
+    this.director = director;
+    this.budget = budget;
+    this.income = income;
+    this.getIntroduction = () => {
+        return `${this.name} ${this.year} ${this.director}`;
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //////////////////////////////////////////
 //               Constructor               //
 // //////////////////////////////////////////
 
-//ex Hotel
-function Hotel (name, rooms, bookedRooms){
-    this.name = name;
-    this.rooms = rooms;
-    this.bookedRooms = bookedRooms;
-    //Kiek kambariu yra laisvu
-    this.availableRooms = function (){
-        return this.rooms - bookedRooms
-    }
-}
+// //ex Hotel
+// function Hotel (name, rooms, bookedRooms){
+//     this.name = name;
+//     this.rooms = rooms;
+//     this.bookedRooms = bookedRooms;
+//     //Kiek kambariu yra laisvu
+//     this.availableRooms = function (){
+//         return this.rooms - bookedRooms
+//     }
+// }
+
+// const guest = new Hotel ("jonas",3, 1);
+
+// console.log(guest);
 
 
-const guest = new Hotel ("jonas",3, 1);
-
-console.log(guest);
 
 
+// //ex Book
+// function Book (name, author, year){
+//     this.name = name;
+//     this.author = author;
+//     this.year = year;
+//     this.printAuthor = function () {
+//         console.log(this.author);
+//     }
+//     this.printOld = function (){
+//         console.log(this.year);
+//     }
+// }
 
+// const book = new Book ("LOTR", "J. R. R. Tolkien", "1954")
 
-//ex Book
-function Book (name, author, year){
-    this.name = name;
-    this.author = author;
-    this.year = year;
-    this.printAuthor = function () {
-        console.log(this.author);
-    }
-    this.printOld = function (){
-        console.log(this.year);
-    }
-}
-
-const book = new Book ("LOTR", "J. R. R. Tolkien", "1954")
-
-console.log(book);
+// console.log(book);
