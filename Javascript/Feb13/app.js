e561dee6b87417e95cf433e339e5c7a8c4a01e0e
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-13 20:05:24 +0200

    minor fixes and rest of code

--- Javascript/Feb13/app.js
+++ Javascript/Feb13/app.js
@@ -2,44 +2,49 @@
 //               FORM                      //
 // //////////////////////////////////////////
 
-
 function Movie(name, year, director, budget, income) {
     this.name = name;
     this.year = year;
     this.director = director;
     this.budget = budget;
     this.income = income;
-    this.getIntroduction = () => {
-        return `${this.name} ${this.year} ${this.director}`;
+    this.getIntroduction = function() {
+      return `${this.name} ${this.year} ${this.director}`;
     };
-    this.getProfit = () => {
-        return this.income - this.budget
-    }
-    this.addToList = () => {
-        // sukurs li elementa prides i sarasa
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    this.getProfit = function() {
+      return this.income - this.budget;
+    };
+    this.addToList = function() {
+      const movieList = document.getElementById('movie-list');
+      const li = document.createElement("li");
+      li.innerHTML = this.name;
+      li.addEventListener("click", () => {
+        alert(this.getIntroduction());
+      });
+      const profitBtn = document.createElement("button");
+      profitBtn.innerHTML = "Profit";
+      profitBtn.addEventListener("click", () => {
+        alert(this.getProfit());
+      });
+      li.appendChild(profitBtn);
+      movieList.appendChild(li);
+    };
+  }
+  
+  const movieForm = document.querySelector("form");
+  movieForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const year = parseInt(document.getElementById("year").value);
+    const director = document.getElementById("director").value;
+    const budget = parseInt(document.getElementById("budget").value);
+    const income = parseInt(document.getElementById("income").value);
+    const movie = new Movie(name, year, director, budget, income);
+    movie.addToList();
+    movieForm.reset();
+  });
+  
 
 // //////////////////////////////////////////
 //               Constructor               //
@@ -59,9 +64,6 @@ function Movie(name, year, director, budget, income) {
 // const guest = new Hotel ("jonas",3, 1);
 
 // console.log(guest);
-
-
-
 
 // //ex Book
 // function Book (name, author, year){
