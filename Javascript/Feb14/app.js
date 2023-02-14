403ea26ac7f39ab7f829d40946a0446d86e1d05c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-14 21:14:00 +0200

    learning local storage cookies

--- Javascript/Feb14/app.js
+++ Javascript/Feb14/app.js
@@ -2,54 +2,68 @@
 //               Coookies                  //
 // //////////////////////////////////////////
 
+const textIn = document.querySelector("#textInput");
+const h2 = document.createElement("h2");
+const saveBtn = document.querySelector("#save");
 
-const form = document.querySelector("form");
 
-form.addEventListener("submit", (event)=>{
+textIn.addEventListener("keyup", (event)=>{
     event.preventDefault();
-    let colorInput = document.getElementById("colorPick");
+    h2.textContent = textIn.value;
+    document.body.append(h2)
+})
 
-    //setting the cookie value
-    document.cookie = `color=${colorInput.value}`;
-
-    // check if there is cookie
-    //dig this string conversion for more info on web after lecture
-    const colorCookie = document.cookie.match(/(^| )color=([^;]+)/);
-    if (colorCookie) {
-
-        // console.log(colorCookie);
-        document.body.style.backgroundColor = colorCookie[2];
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-
-        //Delete button listener
-        deleteButton.addEventListener("click", () => {
-        document.cookie = "max-age=0";
-        document.body.style.backgroundColor = "";
-        document.location.reload();
-        });
-
-        //a wild delete button appears now
-        document.body.appendChild(deleteButton);
-        
-        // hide form
-        form.style.display = "none";
-        
-    }
-    
-    
+saveBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    localStorage.setItem("Input text: ", h2.textContent);
 })
 
 
 
 
-// ivykus formos persikrovimui jeigu cookie buvo surastas pati forma pradingsta o vartotojo ekrano fono spalva tampa jo pasirinkta slapva bei atvaizduojamas mygtukas delete
 
 
 
+///////////////////////////////////////////////////
+
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", (event)=>{
+//     event.preventDefault();
+//     let colorInput = document.getElementById("colorPick");
+
+//     //setting the cookie value
+//     document.cookie = `color=${colorInput.value}`;
+
+//     // check if there is cookie
+//     //dig this string conversion for more info on web after lecture
+//     const colorCookie = document.cookie.match(/(^| )color=([^;]+)/);
+//     if (colorCookie) {
+
+//         // console.log(colorCookie);
+//         document.body.style.backgroundColor = colorCookie[2];
+//         const deleteButton = document.createElement("button");
+//         deleteButton.textContent = "Delete";
+
+//         //Delete button listener
+//         deleteButton.addEventListener("click", () => {
+//         document.cookie = "max-age=0";
+//         document.body.style.backgroundColor = "";
+//         document.location.reload();
+//         });
+
+//         //a wild delete button appears now
+//         document.body.appendChild(deleteButton);
+        
+//         // hide form
+//         form.style.display = "none";
+        
+//     }
+// })
 
 
-// PVZ
+///////////////////////////////////////////////////
+//////////////////////PVZ/////////////////////////
 // const date = new Date('14 Jun 20256 00:00:00 PDT');
 // document.cookie = "name=Domantas";
 // document.cookie = 'lastName=Jonas; expires' + date.toUTCString();
