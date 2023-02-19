b00f2b4a9548f57d2b8db52f491bbf851c0aeeaf
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-19 14:27:50 +0200

    bankomatas

--- Javascript/Feb09/bankomatas.js
+++ Javascript/Feb09/bankomatas.js
@@ -2,16 +2,26 @@ const transactions = [40, 900, -50, -40, 65, 1300, -78, -90, 72, -300, -1, -99];
 
 // 1. Sudėjus visus skaičius kokia suma gaunasi?
 
-sum = transactions.reduce((acc, curent) => acc + curent)
+const sum = transactions.reduce((acc, curent) => acc + curent)
 console.log(sum);
 
 // 2. Kiek kartų buvo įvykdytą transactions į sąskaitą (teigiamų tik reikšmių atsakymas bus 5) ir kokia suma iš viso buvo įnešta į sąskaitą (atsakymas bus 2377 kurį turite parodyti)?
 
-
+const transactionTimes = transactions.filter(tran => tran > 0);
+console.log(transactionTimes.length)
 
 // 3. Kiek kartų buvo nuimta ir kokia bendra suma, tai iš esmės kaip antroje užduotyje tik su nuskaičiuotom, kitaip tariant neigiamom reikšmėm (čia jums gali tekti pamąstyti ar pasigooglinti kaip paversti gautą neigiamą skaičių teigiamu)?
 
+
+const withdrawalTransactions = transactions.filter(transaction => transaction < 0);
+const withdrawalCount = withdrawalTransactions.length;
+const withdrawalTotal = withdrawalTransactions.reduce((acc, current) => acc + Math.abs(current), 0);
+
+console.log(withdrawalCount);
+console.log(withdrawalTotal); 
+
 // 4. Kokia didŽiausia pridėta suma? (pabandykite naudoti if else)
+
 
 // 5. Kokia mažiausia nuimta suma? (pabandykite naudoti ternary operator)
 
