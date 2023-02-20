0a22c735f4d4fb9e2ff78b308339ca26cc3d54e7
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-20 21:13:23 +0200

    promises

--- Javascript/Feb20/app.js
+++ Javascript/Feb20/app.js
@@ -1,21 +1,68 @@
 // //////////////////////////////////////////
+//             Promises PT 2               //
+// //////////////////////////////////////////
+
+
+// //////////////////////////////////////////
+//                  promises               //
+// //////////////////////////////////////////
+
+// function makePizza(toppings) {
+//     return new Promise((resolve, reject) => {
+//       if (toppings && toppings.length > 0) {
+//         const pizza = `Pica su: ${toppings.join(", ")}`;
+//         resolve(pizza);
+//       } else {
+//         reject("Nurodykite bent viena ingredienta.");
+//       }
+//     });
+//   }
+  
+//   makePizza(["pomidorai", "grybai"])
+
+//     .then((result) => console.log(result))
+//     .catch((error) => console.log(error));
+    
+    
+//////////////////////////////////////////////////
+
+// let makePizza = function (toppings) {
+    
+//     new Promise((resolve, reject) => {
+
+//         if(toppings !== "ananasas"){
+//             resolve(console.log(`Tavo pica su ${toppings}`))
+//         }else{
+//             reject("FUJ!!");
+//         }
+    
+    
+//     })
+//     return Promise;
+// }
+// makePizza("Ananasai")
+
+
+
+  
+
+
+// //////////////////////////////////////////
 //                  Async                  //
 // //////////////////////////////////////////
 
-setTimeout(goodMorning, 1000);
-setTimeout(goodAfternoon, 1000);
-setTimeout(goodEvening, 1000);
+// setTimeout(goodMorning, 1000);
+// setTimeout(goodAfternoon, 1000);
+// setTimeout(goodEvening, 1000);
 
+// function goodMorning (){
+//     console.log("good morning")
+// }
 
+// function goodAfternoon (){
+//     console.log("good afternoon")
+// }
 
-function goodMorning (){
-    console.log("good morning")
-}
-
-function goodAfternoon (){
-    console.log("good afternoon")
-}
-
-function goodEvening () {
-    console.log("good evening");
-}
+// function goodEvening () {
+//     console.log("good evening");
+// }
