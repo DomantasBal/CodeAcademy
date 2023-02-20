bc3fa95f64c2d5ffad81153ec186bdedff319e60
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-20 16:12:14 +0200

    adding click next card functionality

--- Javascript/FlashcardGame/cardsReview.js
+++ Javascript/FlashcardGame/cardsReview.js
@@ -1,54 +1,85 @@
 /////////////////////      Review Cards PAGE      /////////////////////
 const frontCard = document.querySelector("#frontOfCard");
 const backCard = document.querySelector("#backOfCard");
-let cardNumberCount = 1;
+const card = document.querySelector(".card");
 
+
+
+//Kai window objekte localstorage yra pakites vykdomas callbackas su gavimu kortos info.
+const retrieveCards = localStorage.getItem('Cards');
+
+//CARDS kur storinamas objektu array su visa info
+const cards = JSON.parse(retrieveCards);
+
+
+
+/////////// Click NEXT Functionality /////////// 
+let cardIndex = 0;
+card.addEventListener("click", () => {
+
+
+        frontCard.textContent = cards[cardIndex].front;
+        backCard.textContent = cards[cardIndex].back;
+        cardIndex++;
+
+        if(cardIndex >= cards.length){
+            cardIndex = 0;
+        }
+
+})   
+
+
+/////////// On LocalStorage Change get DATA  /////////// 
 window.addEventListener("storage", () =>{
     //Kai window objekte localstorage yra pakites vykdomas callbackas su gavimu kortos info.
-    var retrieveCards = localStorage.getItem('Cards');
+    const retrieveCards = localStorage.getItem('Cards');
     
     //CARDS kur storinamas objektu array su visa info
-    var cards = JSON.parse(retrieveCards);
-
-
+    const cards = JSON.parse(retrieveCards);
+    
+    
     // Update CARD TEXT
     frontCard.textContent = cards[0].front;
     backCard.textContent = cards[0].back;
-
-
+    
+    
     // Number of Cards - Desineje
     const numberOfCards = document.querySelector("#numberOfCards");
     numberOfCards.textContent = cards.length;
-
+    
     // What is Card Number?
+    let cardNumberCount = 1;
     const cardNumber = document.querySelector("#cardNumber");
-    console.log(cardNumberCount);
     cardNumber.textContent = `Card # ${cardNumberCount}`;
     cardNumberCount++;
-
+    
+    /////////// Click on card CHANGE Functionality /////////// 
+    
+    
 })
 
 
-
-
+/////////// Flip Functionality /////////// 
 let countClicks = 0;
 const flipCard = document.querySelector("#flipCard");
 flipCard.addEventListener("click", (event) => {
     event.preventDefault();
     countClicks++;
-    
-
-
-    console.log(countClicks);
 
         if (countClicks % 2 === 0) {
-            frontCard.style.display = 'none';
-            backCard.style.display = 'flex';
-        } else {
             backCard.style.display = 'none';
             frontCard.style.display = 'flex';
+
+        } else {
+            frontCard.style.display = 'none';
+            backCard.style.display = 'flex';
         }
 })
+
+
+
+
+
 
 
 
