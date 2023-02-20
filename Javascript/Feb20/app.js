c6df90593b61087a87064649696fb9ccb431f896
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-20 19:30:35 +0200

    ASYNC - Set tiemout

---
+++ Javascript/Feb20/app.js
@@ -0,0 +1,21 @@
+// //////////////////////////////////////////
+//                  Async                  //
+// //////////////////////////////////////////
+
+setTimeout(goodMorning, 1000);
+setTimeout(goodAfternoon, 1000);
+setTimeout(goodEvening, 1000);
+
+
+
+function goodMorning (){
+    console.log("good morning")
+}
+
+function goodAfternoon (){
+    console.log("good afternoon")
+}
+
+function goodEvening () {
+    console.log("good evening");
+}
