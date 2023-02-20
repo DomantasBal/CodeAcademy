ec0b63cb5dd50cf136e436181a7e915bf192979e
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-02-20 12:43:49 +0200

    added count function

--- Javascript/FlashcardGame/app.js
+++ Javascript/FlashcardGame/app.js
@@ -1,15 +1,17 @@
 const saveBtn = document.querySelector("#saveBtn");
 const frontCardText = document.querySelector("#frontCardText");
 const backCardText = document.querySelector("#backCardText");
+let cardNumber = 0;
 const cards = [];
 
 // Surenka info is text area
 saveBtn.addEventListener("click", event =>{
     event.preventDefault();
-   let frontText = frontCardText.value;
-   let backText = backCardText.value;
-   let cardNumber = 0;
-   
+    let frontText = frontCardText.value;
+    let backText = backCardText.value;
+    
+    //Cards counteris
+    cardNumber++;
 
 // Sukuria kortos objekta su constructor funkcija
 function makeCard (frontText, backText, cardNumber){
@@ -21,17 +23,24 @@ function makeCard (frontText, backText, cardNumber){
 // Inicijuoja nauja korta su textarea duomenimis
 let card  = new makeCard (frontText,backText,cardNumber);
 
-//Cards Collection Array
+//Prideda nauja korta i kortu rinkini - Array
 cards.push(card)
-console.log(cards);
 
-//Card number and card length
+// Number of flash cards - Desineje
+const originalCountText = document.querySelector("#numberOfCards");
+const newCountText = `Number of FlashCards: ${cards.length}`;
+originalCountText.textContent = newCountText;
+
+
+
+
 // console.log(cards.length);
-
-
 
 //Event listener END
 })
+
+
+
 
 
 
