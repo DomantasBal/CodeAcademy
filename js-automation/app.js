fa300c4df93e550045bda1db7d9d52a8af48a67e
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-09 20:15:14 +0200

    yarn packages set up lecture

---
+++ js-automation/app.js
@@ -0,0 +1,8 @@
+import 'materialize-css/dist/js/materialize.min.js'
+
+// const test = 'test'
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.parallax')
+  var instances = M.Parallax.init(elems, options)
+})
