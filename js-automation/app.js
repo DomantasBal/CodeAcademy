6cff64e43ba720dfc82bb921416c6f938a71dce3
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-11 19:14:20 +0200

    added materialize js cdn

--- js-automation/app.js
+++ js-automation/app.js
@@ -3,6 +3,6 @@ import 'materialize-css/dist/js/materialize.min.js'
 // const test = 'test'
 
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.parallax')
-  var instances = M.Parallax.init(elems, options)
+  const elems = document.querySelectorAll('.parallax')
+  const instances = M.Parallax.init(elems)
 })
