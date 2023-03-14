ae09f7aa4a749930c2bc5ef0e5dc69489c1ad398
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-14 21:08:48 +0200

    started doing features section

--- Bootstrap/app.js
+++ Bootstrap/app.js
@@ -1,8 +1,1 @@
 import 'bootstrap/dist/js/bootstrap.min.js'
-
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
