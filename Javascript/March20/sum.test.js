96366d5b103df4a43f32a42943610f88290ae63c
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-03-21 18:13:36 +0200

    testing jest continues

--- Javascript/March20/sum.test.js
+++ Javascript/March20/sum.test.js
@@ -3,3 +3,11 @@ describe("sum.js", function () {
     expect(1).toBe(1);
   });
 });
+
+test("empty object equals empty object", () => {
+  const obj = { name: "bob" };
+  // const expectedResult = { name: "tom" };
+  const expectedResult = { name: "bob" };
+
+  expect(obj).toEqual(expectedResult);
+});
