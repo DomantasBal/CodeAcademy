a1f74371470ec102e59a9358615b3f137a2b50a6
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-04-20 21:17:03 +0300

    learning react

--- React/APR20/intro-app/src/components/Counter.jsx
+++ React/APR20/intro-app/src/components/Counter.jsx
@@ -4,12 +4,13 @@ const Counter = ({step}) => {
     const [count, setCount] = useState(0);
 
     const increment = () => setCount(count + step);
+    const decrement = () => setCount(count - step);
 
 return (
     <>
     <h3>Count:{count}</h3>
     <button onClick={increment}>+</button>
-    <button>-</button>
+    <button onClick={decrement}>-</button>
     </>
 )
 
