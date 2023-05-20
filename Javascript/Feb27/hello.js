9fff540d45eb9672c9d30bd420ff01c1fbe78b10
Domantas <92823413+DomantasBal@users.noreply.github.com>
2023-05-20 10:36:49 +0300

    clean up of notes

--- Javascript/Feb27/hello.js
+++
@@ -1,6 +0,0 @@
-import {user} from "./user.js";
-
-document.body.innerHTML = user; // Joe
-
-
-console.log(user)
--- Javascript/March20/index.js
+++
@@ -1,7 +0,0 @@
-function sum(a, b) {
-  return a + b;
-}
-function isGreaterThan(a, b) {
-  return a > b;
-}
-module.exports = { isGreaterThan, sum };
